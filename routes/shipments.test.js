48f80bb2b802441fbd38c70d9598017a14333b95
Diane <hello@dianenguyen.ca>
2023-01-10 16:32:24 -0800

    Wrote scheme for shipment

---
+++ routes/shipments.test.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const request = require("supertest");
+const app = require("../app");
+
+
+describe("POST /", function () {
+  test("valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  /** Should throw error if no request body */
+  test("throws error if empty request body", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send();
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  /** Should throw error if missing inputs */
+  test("throws error if missing an input", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        "productId": 2000,
+        "name": "diane"
+      });
+    expect(resp.statusCode).toEqual(400);
+
+    expect(resp.body.error.message).toEqual([
+			"instance requires property \"addr\"",
+			"instance requires property \"zip\""
+		]);
+  });
+});
