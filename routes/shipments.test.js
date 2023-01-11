f4a29166511a14578cbad5b0c6322e9dd73f39f9
Your Full Name <you@youremail.com>
2023-01-10 20:29:08 -0500

    finished 2nd code review

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const shipItApi = require("../shipItApi");
+shipItApi.shipProduct = jest.fn();
+
 const request = require("supertest");
 const app = require("../app");
 
-
 describe("POST /", function () {
   test("valid", async function () {
+    shipItApi.shipProduct.mockReturnValue(5555);
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,30 +17,28 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 5555 });
   });
 
   /** Should throw error if no request body */
   test("throws error if empty request body", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send();
+    const resp = await request(app).post("/shipments").send();
+
     expect(resp.statusCode).toEqual(400);
   });
 
   /** Should throw error if missing inputs */
   test("throws error if missing an input", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send({
-        "productId": 2000,
-        "name": "diane"
-      });
+    const resp = await request(app).post("/shipments").send({
+      productId: 2000,
+      name: "diane",
+    });
+
     expect(resp.statusCode).toEqual(400);
 
     expect(resp.body.error.message).toEqual([
-			"instance requires property \"addr\"",
-			"instance requires property \"zip\""
-		]);
+      'instance requires property "addr"',
+      'instance requires property "zip"',
+    ]);
   });
 });
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,18 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
 
+const { SHIPIT_SHIP_URL, shipProduct } = require("./shipItApi");
 
 test("shipProduct", async function () {
+  axiosMock.onPost(SHIPIT_SHIP_URL).reply(200, {
+    receipt: {
+      shipId: 5555,
+    },
+  });
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +20,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(5555);
 });
